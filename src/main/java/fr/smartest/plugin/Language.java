434b63292bf2498101276fd9095b493d5e8fdf07
FlorianLehmann <Lehmann.florian@yandex.com>
2018-06-04 11:16:51 +0200

    PNSE-13 change to method to get a Diff

--- src/main/java/fr/smartest/plugin/Language.java
+++ src/main/java/fr/smartest/plugin/Language.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public interface Language extends Plugin {
 
     void setUp(List<Module> modules);
-    Set<Test> getTestsRelatedToChanges(String scope, Set<String> fileDiff);
+    Set<Test> getTestsRelatedToChanges(String scope, Set<Diff> diff);
     void save();
 
 }
