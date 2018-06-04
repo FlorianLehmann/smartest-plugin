fb2126739a258adc04cbf6064e4be2e31dcc6d04
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-06-04 08:10:11 +0200

    Add implementation to exceptions.

--- src/main/java/fr/smartest/exceptions/PluginException.java
+++ src/main/java/fr/smartest/exceptions/PluginException.java
@@ -2,4 +2,22 @@ package fr.smartest.exceptions;
 
 public class PluginException extends SmartestException {
 
+    public PluginException() {
+    }
+
+    public PluginException(String message) {
+        super(message);
+    }
+
+    public PluginException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PluginException(Throwable cause) {
+        super(cause);
+    }
+
+    public PluginException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/main/java/fr/smartest/exceptions/SmartestException.java
+++ src/main/java/fr/smartest/exceptions/SmartestException.java
@@ -1,5 +1,22 @@
 package fr.smartest.exceptions;
 
 public class SmartestException extends Exception {
+    public SmartestException() {
+    }
 
+    public SmartestException(String message) {
+        super(message);
+    }
+
+    public SmartestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SmartestException(Throwable cause) {
+        super(cause);
+    }
+
+    public SmartestException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
