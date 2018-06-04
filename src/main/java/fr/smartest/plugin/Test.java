69dd67ad35ef1096086f58e4b6c1c805a21b0581
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-06-04 17:42:51 +0200

    Added value to Priority.

--- src/main/java/fr/smartest/plugin/Test.java
+++ src/main/java/fr/smartest/plugin/Test.java
@@ -1,11 +1,23 @@
 package fr.smartest.plugin;
 
+import java.util.Comparator;
+
 public interface Test {
 
-    enum Priority {
-        HIGH,
-        MEDIUM,
-        LOW
+    enum Priority implements Comparable<Priority> {
+        HIGH(10),
+        MEDIUM(5),
+        LOW(2);
+
+        private final int value;
+
+        Priority(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 
     Priority getPriority();
