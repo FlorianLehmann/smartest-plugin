0b972e8dde65fa546469aa2725aefd847ca8cc58
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-06-05 15:02:39 +0200

    Removed TestFramework init.

--- src/main/java/fr/smartest/plugin/TestFramework.java
+++ src/main/java/fr/smartest/plugin/TestFramework.java
@@ -2,13 +2,11 @@ package fr.smartest.plugin;
 
 import fr.smartest.exceptions.TestFrameworkException;
 
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Set;
 
 public interface TestFramework extends Plugin {
 
-    void setUp(Path path, List<Module> modules);
-    Set<TestReport> run(Set<Test> tests) throws TestFrameworkException;
+    Set<TestReport> run(Set<Test> tests, List<Module> modules) throws TestFrameworkException;
 
 }
